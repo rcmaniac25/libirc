722bbe628433aa3633773fd5c7faadb70d52aa2c
jeffm2501 <jeffm2501@79e00ee0-3d45-4b12-aa0e-f1414af056b2>
2004-09-27 06:27:32 +0000

    IRC connection sample
    git-svn-id: http://svn.code.sf.net/p/libirc/code/trunk@66 79e00ee0-3d45-4b12-aa0e-f1414af056b2

---
+++ libirc/examples/simpleIRCConnect/readme.txt
@@ -0,0 +1,3 @@
+SimpleIRCConnect
+
+Sample app that shows how to use libIRC to conenct to an IRC server as a client.
---
+++ libirc/examples/simpleIRCConnect/src/simpleIRCConnect.cpp
@@ -0,0 +1,24 @@
+/* libIRC
+* Copyright (c) 2004 Christopher Sean Morrison
+*
+* This package is free software;  you can redistribute it and/or
+* modify it under the terms of the license found in the file
+* named LICENSE that should have accompanied this file.
+*
+* THIS PACKAGE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+* IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+* WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+*/
+
+// simple IRC connection sample
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "libIRC.h"
+
+
+void main ( void )
+{
+
+};
