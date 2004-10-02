98ee8708941df19436b18cebdc301f66b018eb17
jeffm2501 <jeffm2501@79e00ee0-3d45-4b12-aa0e-f1414af056b2>
2004-10-02 16:42:36 +0000

    old code go byebye
    git-svn-id: http://svn.code.sf.net/p/libirc/code/trunk@110 79e00ee0-3d45-4b12-aa0e-f1414af056b2

--- libirc/src/old_code/funcptrs.h
+++
@@ -1,8 +0,0 @@
-#ifndef __FUNCPTRS_H__
-#define __FUNCPTRS_H__
-
-typedef int (*key_cmp_func_t)(void* key_left, void* key_right);
-typedef unsigned int (*key_hash_func_t)(void* value);
-typedef void (*destructor_t)(void* object);
-
-#endif /* __FUNCPTRS_H__ */
--- libirc/src/old_code/irc_version.h
+++
@@ -1,3 +0,0 @@
-#define LIBIRC_NAME	"libirc"
-#define LIBIRC_MAJOR	0
-#define LIBIRC_MINOR	5
