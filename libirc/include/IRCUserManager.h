7f5a43564254f957f7cd2cedbe07fb7edc650bea
jeffm2501 <jeffm2501@79e00ee0-3d45-4b12-aa0e-f1414af056b2>
2004-10-08 02:50:03 +0000

    redone user manager API
    git-svn-id: http://svn.code.sf.net/p/libirc/code/trunk@122 79e00ee0-3d45-4b12-aa0e-f1414af056b2

--- libirc/include/IRCUserManager.h
+++ libirc/include/IRCUserManager.h
@@ -1,7 +1,7 @@
 /* libIRC
 * Copyright (c) 2004 Christopher Sean Morrison
 *
-* This package is free software;  you can redistribute it and/or
+* This package is free software; you can redistribute it and/or
 * modify it under the terms of the license found in the file
 * named LICENSE that should have accompanied this file.
 *
@@ -19,8 +19,6 @@
 #include <vector>
 #include <string>
 #include <map>
-
-class IRCChannelManager;
 
 typedef struct 
 {
@@ -46,12 +44,32 @@ typedef struct
 
 typedef struct 
 {
-	int									id;
-	std::string					name;
-	trIRCUserPermisions	perms;
-}trIRCUserChannelRecord;
+	std::string mode;
+	bool				allowColors;
+	bool				forwarded;
+	bool				inviteOnly;
+	bool				anyInvite;
+	bool				juped;
+	int					userLimit;
+	bool				moderated;
+	bool				externalMessages;
+	bool				permanent;
+	bool				regForVoice;
+	bool				regOnly;
+	bool				secret;
+	bool				reducedModeraton;
+}trIRCChannelPermisions;
 
-typedef std::vector<trIRCUserChannelRecord> tvIRCUserChannelList;
+typedef struct 
+{
+	int																			id;
+	std::string															name;
+	trIRCChannelPermisions									perms;
+	std::vector<int>												users;
+	std::map<int,trIRCChannelUserPermisions>userPerms;
+	std::string															lastMessage;
+	int																			lastMessageUser;
+}trIRChannelRecord;
 
 typedef struct 
 {
@@ -63,13 +81,8 @@ typedef struct
 	std::string							lastMessage;
 
 	trIRCUserPermisions			perms;
-	tvIRCUserChannelList		channels;
-
-	string_list getChannels ( void );
+	std::vector<int>				channels;
 }trIRCUserRecord;
-
-typedef std::map<std::string,int> tmIRCUserIDNameMap;
-typedef std::map<int,trIRCUserRecord> tmIRCUserIDMap;
 
 class IRCUserManager
 {
@@ -77,20 +90,121 @@ public:
 		IRCUserManager();
 		~IRCUserManager();
 
-		void setChannelManager ( IRCChannelManager *chanMan ){channelManager = chanMan;}
-
+		// user info API
 		int getUserID ( std::string &name );
-		trIRCUserRecord& getUserInfo ( int id );
-		trIRCUserRecord& getUserInfo ( std::string &name );
+		std::string getUserNick ( int id );
 
+		std::string getUserHost ( int id );
+		std::string getUserHost ( std::string &name );
+		void setUserHost ( int id, std::string &host );
+		void setUserHost ( std::string &name, std::string &host );
+
+		std::string getUserLastMessage ( int id );
+		std::string getUserLastMessage ( std::string &name );
+
+		int getUserLastMessageChannel ( int id );
+		int getUserLastMessageChannel ( std::string &name );
+		std::string getUserLastMessageChannelName ( int id );
+		std::string getUserLastMessageChannelName ( std::string &name );
+
+		trIRCUserPermisions getUserPerms ( int id );
+		trIRCUserPermisions getUserPerms ( std::string &name );
+
+		std::vector<int> listUsers ( void );
+		std::vector<std::string> listUserNames ( void );
+
+		// user in channel API
+		bool userHasChannels ( int id );
+		bool userHasChannels ( std::string &name );
+
+		bool userInChannel ( int id, int channel );
+		bool userInChannel ( int id, std::string& channel );
+		bool userInChannel ( std::string &name, int channel );
+		bool userInChannel ( std::string &name, std::string& channel );
+
+		trIRCChannelUserPermisions getUserChannelPerms ( int id, int channel );
+		trIRCChannelUserPermisions getUserChannelPerms ( int id, std::string& channel );
+		trIRCChannelUserPermisions getUserChannelPerms ( std::string &name, int channel );
+		trIRCChannelUserPermisions getUserChannelPerms ( std::string &name, std::string& channel );
+
+		bool userIsIdentified ( int id );
+		bool userIsIdentified ( std::string &name );
+
+		bool userIsOp ( int id, int channel );
+		bool userIsOp ( int id, std::string& channel );
+		bool userIsOp ( std::string &name, int channel );
+		bool userIsOp ( std::string &name, std::string& channel );
+
+		bool userHasVoice ( int id, int channel );
+		bool userHasVoice ( int id, std::string& channel );
+		bool userHasVoice ( std::string &name, int channel );
+		bool userHasVoice ( std::string &name, std::string& channel );
+
+		std::vector<int> listUserChannels ( int id );
+		std::vector<int> listUserChannels ( std::string &name );
+		std::vector<std::string> listUserChannelNames ( int id );
+		std::vector<std::string> listUserChannelNames ( std::string &name );
+
+		// channel API
+		int getChannelID ( std::string &channel );
+		std::string getChannelName ( int id );
+
+		trIRCChannelPermisions getChannelPerms ( int id );
+		trIRCChannelPermisions getChannelPerms ( std::string &channel );
+
+		std::string getChannelLastMessage ( int id );
+		std::string getChannelLastMessage ( std::string &channel );
+
+		int getChannelLastMessageUser ( int id );
+		int getChannelLastMessageUser ( std::string &channel );
+		std::string getChannelLastMessageUserName ( int id );
+		std::string getChannelLastMessageUserName ( std::string &channel );
+
+		std::vector<int> listChannelUsers ( int id );
+		std::vector<int> listChannelsUser ( std::string &name );
+		std::vector<std::string> listChanneUserlNames ( int id );
+		std::vector<std::string> listChannelUserNames ( std::string &name );
+
+
+		// state update from the IRC data stream
+		void userJoinChannel ( int user,  int channel );
+		void userJoinChannel ( int user, std::string &channel );
+		void userJoinChannel ( std::string &user, int channel );
 		void userJoinChannel ( std::string &user, std::string &channel );
+
+		void userJoinChannel ( int user,  int channel );
+		void userPartChannel ( int user, std::string &channel );
+		void userPartChannel ( std::string &user, int channel );
 		void userPartChannel ( std::string &user, std::string &channel );
 
+		void nickChange ( std::string &oldNick, std::string &newNick );
+
+		void messageReceved ( std::string &target, std::string &source, std::string &message );
+
+		void modeReceved ( std::string &target, std::string &source, std::string &mode );
+
+
+		// utilitys
+		void purgeNonChannelUsers ( void );
+		void purgeLastMessages ( void );
+		void setPurgeOnLastPart ( bool purge ){autoPurgeOnLastPart = purge;}
+		bool purgeOnLastPart ( void ){return autoPurgeOnLastPart;}
+
 protected:
-	IRCChannelManager		*channelManager;
-	tmIRCUserIDMap			users;
-	tmIRCUserIDNameMap	usernames;
-	unsigned int				lastID;
+	trIRCUserRecord& getUserInfo ( int id );
+	trIRCUserRecord& getUserInfo ( std::string &name );
+
+	trIRChannelRecord& getChannelInfo ( int id );
+	trIRChannelRecord& getChannelInfo ( std::string &channel );
+
+	std::string getCleanNick ( std::string &nick );
+
+	std::map<int,trIRCUserRecord>	users;
+	std::map<int,trIRChannelRecord> channels;
+
+	bool			autoPurgeOnLastPart;
+	int				lastUserID;
+	int				lastChannelID;
 };
 
 #endif//_IRC_USER_MANAGER_
