74e5a24783ac142c4a8b61c54ea3deab58d6c140
JeffM2501 <JeffM2501@79e00ee0-3d45-4b12-aa0e-f1414af056b2>
2007-11-19 22:13:53 +0000

    when checking for "dunno" responces, make sure we have some before we try to get a random one
    git-svn-id: http://svn.code.sf.net/p/libirc/code/trunk@340 79e00ee0-3d45-4b12-aa0e-f1414af056b2

--- libirc/examples/stupidBot/src/stupidBot.cpp
+++ libirc/examples/stupidBot/src/stupidBot.cpp
@@ -584,14 +584,17 @@ void channelMessage ( trClientMessageEventInfo *info )
 				// see if it's a factoid
 				if (!callBotCommand("factoid",info->source,info->from,info))
 				{
-					// it's not, we dono what it is, but it was addressed to us
-					std::string	dono = getRandomString(theBotInfo.unknownResponces);
+					if (theBotInfo.unknownResponces.size())
+					{
+						// it's not, we dono what it is, but it was addressed to us
+						std::string	dono = getRandomString(theBotInfo.unknownResponces);
 
-					dono = string_util::replace_all(dono,"%u",info->from);
-					dono = string_util::replace_all(dono,"%c",info->target);
-					dono = string_util::replace_all(dono,"%b",client.getNick());
+						dono = string_util::replace_all(dono,"%u",info->from);
+						dono = string_util::replace_all(dono,"%c",info->target);
+						dono = string_util::replace_all(dono,"%b",client.getNick());
 
-					client.sendMessage(info->target,dono);
+						client.sendMessage(info->target,dono);
+					}
 				}
 			}
 		}
